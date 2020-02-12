5157b856da308c60c06a1dd98eda81f39bde8aa3
oohira <oohira@ekzm.co.jp>
2020-02-12 09:17:36 +0900

    add:hooks

--- src/components/entry/entry.stories.js
+++ src/components/entry/entry.stories.js
@@ -5,46 +5,45 @@ import ReactExample from '@c/example/ReactExample'
 
 export const entryList = () => {
   let code = `import axios from 'axios'
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import _ from 'lodash'
 
-class EntryList extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      list: []
-    }
-  }
+const EntryList = () => {
+  // state
+  const [list, setList] = useState([])
 
-  async componentDidMount() {
-    let { data } = await axios.get('https://api.noidea.xyz/fake/nicknames.json')
-    let newList = []
-    _.times(20, n => {
-      let names = [_.sample(data).name, _.sample(data).name]
-      newList.push({
-        id: n+1,
-        name: names.join(_.sample(['な','の','に']))
+  // mounted
+  useEffect(() => {
+    const getData = async () => {
+      let { data } = await axios.get('https://api.noidea.xyz/fake/nicknames.json')
+      let newList = []
+      _.times(20, n => {
+        let names = [_.sample(data).name, _.sample(data).name]
+        newList.push({
+          id: n+1,
+          name: names.join(_.sample(['な','の','に']))
+        })
       })
-    })
-    this.setState({ list: newList })
-  }
+      setList(newList)
+    }
+    getData()
+  }, [])
 
-  render() {
-    return (
-      <>
-        <table className="table is-fullwidth">
-          <tbody>
-            {this.state.list.map( i => {
-              return <tr key={i.id}>
-                <th>{i.id}</th>
-                <td>{i.name}</td>
-              </tr>
-            })}
-          </tbody>
-        </table>
-      </>
-    )
-  }
+  // template
+  return (
+    <>
+      <table className="table is-fullwidth">
+        <tbody>
+          {list.map( i => {
+            return <tr key={i.id}>
+              <th>{i.id}</th>
+              <td>{i.name}</td>
+            </tr>
+          })}
+        </tbody>
+      </table>
+    </>
+  )
 }
 
 export default EntryList`
